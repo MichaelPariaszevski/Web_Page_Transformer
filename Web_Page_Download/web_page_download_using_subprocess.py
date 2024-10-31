d6f1bebbfef9d70bf4c2e4ba6df934eb333777c5
MichaelPariaszevski <mpariaszevski@gmail.com>
2024-10-31 10:19:35 -0700

    Add robots.txt and requirements.txt files

---
+++ Web_Page_Download/web_page_download_using_subprocess.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def download_web_page(url: str, output_dir: str = "wget_web_page"): 
+    try:
+        command = ["wget", "-p", "-k", url, "-P", f"./{output_dir}/"]
+        subprocess.run(command) 
+        url_new = url.replace("https://", "")
+        web_page_dir = f"./{output_dir}/{url_new}/index.html"
+        return {"is_successful": True, "web_page_dir": web_page_dir}
+    except Exception: 
+        raise Exception
+    
+def download_web_page_mozilla_auth(url: str, output_dir: str = "wget_web_page"): 
+    try:
+        command = ["wget", "--no-proxy", "-p", "-k", "-U", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4", url, "-P", f"./{output_dir}/"]
+        subprocess.run(command) 
+        url_new = url.replace("https://", "")
+        web_page_dir = f"./{output_dir}/{url_new}/index.html"
+        return {"is_successful": True, "web_page_dir": web_page_dir}
+    except Exception: 
+        raise Exception
